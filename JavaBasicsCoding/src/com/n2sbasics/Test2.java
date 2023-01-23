9e8cd394cf62aaafa60f06a76898bab122067a99
SharmeelaKanna <123007093+SharmeelaKanna@users.noreply.github.com>
2023-01-23 00:16:59 -0500

    Test2

---
+++ JavaBasicsCoding/src/com/n2sbasics/Test2.java
@@ -0,0 +1,14 @@
+package com.n2sbasics;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Example e = new Example();
+		
+		Example2 e2 = new Example2();
+		
+	}
+
+}
