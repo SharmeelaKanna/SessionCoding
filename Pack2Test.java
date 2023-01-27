b09b9c4d02ff0d0bfd44f27bc40b45f83f35a9d7
SharmeelaKanna <123007093+SharmeelaKanna@users.noreply.github.com>
2023-01-26 23:41:32 -0500

    Pack 2

---
+++ Pack2Test.java
@@ -0,0 +1,14 @@
+package com.pack2;
+
+import pack1.Pack1Sample;
+
+public class Pack2Test extends Pack1Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pack2Test obj = new Pack2Test();
+		obj.method1();
+		obj.method2();
+	}
+
+}
