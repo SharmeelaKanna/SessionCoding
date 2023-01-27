c0b4bbdd1812a1f3d970419613591afe758a8bb7
SharmeelaKanna <123007093+SharmeelaKanna@users.noreply.github.com>
2023-01-26 23:17:09 -0500

    Interface2

---
+++ JavaBasicsCoding/src/com/n2sbasics/Ifc2.java
@@ -0,0 +1,12 @@
+package com.oops;
+
+public interface Ifc2 extends Ifc3 {
+	
+	public static final int ifc2_y = 5; 
+	
+	public abstract int sub(int x, int y); 
+	
+	//public abstract int add(int x, int y); 
+}
+
+
