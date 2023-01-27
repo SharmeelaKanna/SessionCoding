e74a06fe984c9f465c600bdbfdfcda667a2facbf
SharmeelaKanna <123007093+SharmeelaKanna@users.noreply.github.com>
2023-01-26 23:16:09 -0500

    Interface1

---
+++ JavaBasicsCoding/src/com/n2sbasics/Ifc1.java
@@ -0,0 +1,20 @@
+package com.oops;
+
+public class Ifc1 {
+
+public static int ifc1_x = 5; 
+
+	public abstract int add(int x, int y);
+	
+	
+	public default void test1() {
+		
+	}
+	
+	public static void test2() {
+		
+	}
+	
+	public void test3();
+
+}
