a246279b35b08dd15714da5aac6c39987223abd0
SharmeelaKanna <123007093+SharmeelaKanna@users.noreply.github.com>
2023-02-10 10:50:01 -0500

    methodref3

---
+++ methodref/StaticMethodRef.java
@@ -0,0 +1,16 @@
+package methodref;
+
+public class StaticMethodRef {
+	public static void main(String[] args) {
+		Payment p = StaticMethodRef::creditPay;
+		p.pay();
+	}
+	
+	public static void creditPay() {
+		System.out.println("Paying through credit card");
+	}
+}
+
+interface Payment {
+	void pay();
+}
