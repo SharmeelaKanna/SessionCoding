0e28e315f62480767c2849fd8a359f52e9fbeec0
SharmeelaKanna <123007093+SharmeelaKanna@users.noreply.github.com>
2023-02-10 10:48:15 -0500

    methodref1

---
+++ methodref/ConstructorMethodRef.java
@@ -0,0 +1,24 @@
+package methodref;
+
+public class ConstructorMethodRef {
+	
+	public static void main(String[] args) {
+
+		Message m = Display::new;
+		m.getMessage("Hello Constructor method reference call");
+
+	}
+
+}
+
+class Display {
+	Display(String msg) {
+		System.out.println(msg);
+	}
+}
+
+
+interface Message {
+	Display getMessage(String msg);
+}
+
